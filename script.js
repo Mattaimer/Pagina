bd101e90d8d747c392b2c95b3478cf2a54880a53
Mattaimer <147774122+Mattaimer@users.noreply.github.com>
2025-05-03 07:55:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var donationToggle = document.getElementById('donationToggle');
-    var buttonTexts = ['Donation', 'Donación'];
-    var index = 0;
 
-    function toggleButtonText() {
-        donationToggle.innerText = buttonTexts[index];
-        index = (index + 1) % buttonTexts.length;
-    }
-
-    // Llama a la función cada 2 segundos (2000 milisegundos)
-    setInterval(toggleButtonText, 2000);
-});
